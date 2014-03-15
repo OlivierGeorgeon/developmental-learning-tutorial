fc194909c13bca940c20908e847b32208aeec6e6
olivier.georgeon <olivier.georgeon@gmail.com>
2014-03-15 16:00:59 +0000

    Save Agent2 and Environment4.

--- TD/src/main/Main.java
+++ TD/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import Agents.Agent;
 import Agents.Agent1;
+import Agents.Agent2;
 import Environments.Environment1;
 import Environments.Environment;
 import Environments.Environment2;
@@ -16,17 +17,19 @@ public class Main {
 	public static Experience e2 = Experience.create("e2");
 	public static Result r1 = new Result("r1");
 	public static Result r2 = new Result("r2");
-	public static Interaction i11 = Interaction.create(e1.getLabel() + r1.getLabel(), 1);
-	public static Interaction i12 = Interaction.create(e1.getLabel() + r2.getLabel(), -1);
-	public static Interaction i21 = Interaction.create(e2.getLabel() + r1.getLabel(), 1);
-	public static Interaction i22 = Interaction.create(e2.getLabel() + r2.getLabel(), -1);
+	public static Interaction i11 = Interaction.createPrimitiveInteraction(e1, r1, -1);
+	public static Interaction i12 = Interaction.createPrimitiveInteraction(e1, r2, 1);
+	public static Interaction i21 = Interaction.createPrimitiveInteraction(e2, r1, -1);
+	public static Interaction i22 = Interaction.createPrimitiveInteraction(e2, r2, 1);
 
 	public static void main(String[] args){
 		
-		Agent agent = new Agent1();
+		//Agent agent = new Agent1();
+		Agent agent = new Agent2();
+		
 		//Environment env = new Environment1();
-		Environment env = new Environment2();
-		//Environment env = new Environment3();
+		//Environment env = new Environment2();
+		Environment env = new Environment3();
 		Experience experience = e1;
 		Result result = r1;
 		
--- TD/src/primitives/Interaction.java
+++ TD/src/primitives/Interaction.java
@@ -1,6 +1,8 @@
 package primitives;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Interaction {
@@ -8,6 +10,26 @@ public class Interaction {
 	
 	private String label;
 	private int value;
+	
+	private Experience experience;
+	private Result result;
+	private Interaction preInteraction;
+	private Interaction postInteraction;
+	
+	public static Interaction createPrimitiveInteraction(Experience experience, Result result, int value){
+		Interaction interaction = create(experience.getLabel() + result.getLabel(), value); 
+		interaction.setExperience(experience);
+		interaction.setResult(result);
+		return interaction;
+	}
+	
+	public static Interaction createOrGetCompositeInteraction(Interaction preInteraction, Interaction postInteraction){
+		int value = preInteraction.getValue() + postInteraction.getValue();
+		Interaction interaction = create(preInteraction.getLabel() + postInteraction.getLabel(), value); 
+		interaction.setPreInteraction(preInteraction);
+		interaction.setPostInteraction(postInteraction);
+		return interaction;
+	}
 	
 	public static Interaction create(String label, int value){
 		if (!INTERACTIONS.containsKey(label))
@@ -17,6 +39,14 @@ public class Interaction {
 	
 	public static Interaction get(String label){
 		return INTERACTIONS.get(label);
+	}
+	
+	public static List<Interaction> getActivatedInteractions(Interaction interaction){
+		List<Interaction> activatedInteractions = new ArrayList<Interaction>();
+		for (Interaction activatedInteraction : INTERACTIONS.values())
+			if (interaction==activatedInteraction.getPreIntearction())
+				activatedInteractions.add(activatedInteraction);
+		return activatedInteractions;
 	}
 	
 	private Interaction(String label, int value){
@@ -30,6 +60,38 @@ public class Interaction {
 	
 	public int getValue(){
 		return this.value;
+	}
+
+	public Experience getExperience() {
+		return experience;
+	}
+
+	public void setExperience(Experience experience) {
+		this.experience = experience;
+	}
+
+	public Result getResult() {
+		return result;
+	}
+
+	public void setResult(Result result) {
+		this.result = result;
+	}
+
+	public Interaction getPreIntearction() {
+		return preInteraction;
+	}
+
+	public void setPreInteraction(Interaction preInteraction) {
+		this.preInteraction = preInteraction;
+	}
+
+	public Interaction getPostInteraction() {
+		return postInteraction;
+	}
+
+	public void setPostInteraction(Interaction postInteraction) {
+		this.postInteraction = postInteraction;
 	}
 
 }
