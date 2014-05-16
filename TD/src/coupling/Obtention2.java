73265132af58107943b01fbc4039979c5ae316e3
olivier.georgeon <olivier.georgeon@gmail.com>
2014-05-16 12:38:35 +0000

    The classes Coupling were deleted.

---
+++ TD/src/coupling/Obtention2.java
@@ -0,0 +1,26 @@
+package coupling;
+
+import coupling.interaction.Interaction2;
+
+/**
+ * An Interaction3 is an Interaction2 with a weight.
+ * @author Olivier.
+ */
+public class Obtention2 implements Obtention {
+	
+	protected Interaction2 interaction;
+	
+	public Obtention2(Interaction2 interaction){
+		this.interaction = interaction;
+	}
+	
+	public Interaction2 getInteraction(){
+		return this.interaction;
+	}
+
+	@Override
+	public String getLabel() {
+		return this.interaction.getLabel();
+	}
+
+}
