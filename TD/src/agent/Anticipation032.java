97330d885eaeb6f5907bfe0dc831e098e1f735df
olivier.georgeon <olivier.georgeon@gmail.com>
2014-09-16 12:54:18 +0000

    Anticipation032 was added.

---
+++ TD/src/agent/Anticipation032.java
@@ -0,0 +1,35 @@
+package agent;
+
+import coupling.interaction.Interaction030;
+import coupling.interaction.Interaction032;
+
+public class Anticipation032 extends Anticipation030 {
+
+	int proclivity;
+
+	public Anticipation032(Interaction032 interaction, int proclivity){
+		super(interaction);
+		this.proclivity = proclivity;
+	}
+
+	public int compareTo(Anticipation anticipation){
+		return new Integer(((Anticipation032)anticipation).getProclivity()).compareTo(this.proclivity);
+	}
+
+	public boolean equals(Object otherProposition){
+		return ((Anticipation032)otherProposition).getInteraction() == this.getInteraction();
+	}
+	
+	public int getProclivity() {
+		return proclivity;
+	}
+
+	public void addProclivity(int proclivity) {
+		this.proclivity += proclivity;
+	}
+	
+	@Override
+	public String toString(){
+		return this.getInteraction().getLabel() + " proclivity " + this.getProclivity();
+	}
+}
