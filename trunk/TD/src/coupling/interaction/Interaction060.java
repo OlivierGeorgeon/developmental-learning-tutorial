01db5001d4658a4ed9035262bd68bc040113b144
olivier.georgeon@gmail.com <olivier.georgeon@gmail.com@54ff396d-ef74-87c4-ee04-a303d146042e>
2015-01-28 15:41:06 +0000

    Interaction060 have a persistency attribute.

--- trunk/TD/src/coupling/interaction/Interaction060.java
+++ trunk/TD/src/coupling/interaction/Interaction060.java
@@ -1,13 +1,30 @@
 package coupling.interaction;
 
+import existence.Existence060;
+
 /**
- * An interaction060 is an Interaction040 that can return its length.
+ * An interaction060 is an Interaction040 that has a persistency value and that can return its length.
  * The length is used for tracing.
  */
 public class Interaction060 extends Interaction040 {
+	
+	private int persistency = 0;
 
 	public Interaction060(String label){
 		super(label);
+	}
+	
+	public void incPersistency(){
+		if (this.persistency > Existence060.UNPERSISTENT)
+			this.persistency++;
+	}
+	
+	public void setPersistency(int peristency){
+		this.persistency = peristency;
+	}
+	
+	public int getPersistency(){
+		return this.persistency;
 	}
 	
 	public int getLength(){
